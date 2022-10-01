999e8a035102dbd232e68eed17dd437361d972a4
ecemcakmak <eng.ecemcakmak@gmail.com>
2022-10-01 17:40:28 +0300

    add: kitapliste.js sayfası tamamlandı satırlarına link verilecek

--- my-app/src/pages/kitapliste.js
+++ my-app/src/pages/kitapliste.js
@@ -18,24 +18,24 @@ export default function KitapListe() {
                 <Table className="table table-image table__list">
                   <thead>
                     <tr>
-                      <th scope="col">Day</th>
-                      <th scope="col">Image</th>
-                      <th scope="col">Article Name</th>
-                      <th scope="col">Author</th>
-                      <th scope="col">Words</th>
-                      <th scope="col">Shares</th>
+                   
+                      <th scope="col">Kitap Kapağı</th>
+                      <th scope="col">Kitap Evi</th>
+                      <th scope="col">Kitap Adı</th>
+                      <th scope="col">Yazar</th>
+                      
+                      <th scope="col">Basım Tarihi</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody> 
                     <tr>
-                      <th scope="row">2</th>
                       <td className="w-25">
                         <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg" className="img-fluid img-thumbnail" alt="Sheep"/>
                       </td>
-                      <td>Bootstrap Grid 4 Tutorial and Examples</td>
-                      <td>Cristina</td>
-                      <td>1.434</td>
-                      <td>3.417</td>
+                      <td>İnkılap</td>
+                      <td>Tanrılar Okulu</td>
+                      <td>Stefano D'Anna</td>
+                      <td>7.2016</td>
                     </tr>
                   </tbody>
                 </Table>   
