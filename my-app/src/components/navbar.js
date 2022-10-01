b84078bb959fae9b16386b3dbef55f6e29188d05
ecemcakmak <eng.ecemcakmak@gmail.com>
2022-10-01 19:32:56 +0300

    add: navbara search bar eklendi

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Input from 'react-bootstrap/InputGroup'
+import Container from 'react-bootstrap/Container';
 
 
 
 export default function Navbar(){
     return <nav className="nav">
-        <Link to="/" className="site-title">
+            <Container>
 
-        </Link>
-        <ul>
-            <CustomLink to="/">Login</CustomLink>
-            <CustomLink to="/profile">Profile</CustomLink>
-            <CustomLink to="/kitapliste">Kitap Liste</CustomLink>
-            <CustomLink to="/kitapdetay">Kitap Detay</CustomLink>
-        </ul>
+<Row>
+  <Col className="col-6">
+  <Link to="/" classNameName="site-title">
+
+</Link>
+<ul>
+    <CustomLink to="/">Login</CustomLink>
+    <CustomLink to="/profile">Profile</CustomLink>
+    <CustomLink to="/kitapliste">Kitap Liste</CustomLink>
+    <CustomLink to="/kitapdetay">Kitap Detay</CustomLink>
+</ul>
+  </Col>
+  <Col className="col-6">
+  <div className="input-group">
+            <div className="form-outline">
+                <Input type="search" id="form1" className="form-control" />
+            </div>
+            <button type="button" className="button button__fav btn btn-primary">
+              ara
+            </button>
+            </div>
+  </Col>
+</Row>
+
+</Container>
+     
+
     </nav>
 }
 
